f328c2d9b3eeef78d5c4dfb2a25f78a278b1019f
Julian Canepa <jjlcanepa@gmail.com>
2022-01-27 16:30:53 -0800

    Update funciton, running a larger number.

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 '''
 A recursive solution with complexity of:
 O(Φ^n) where Φ = (√5 + 1)/2
-fib(n) = {
-    0                       n=0
-    1                       n=1
-    fib(n-2) + fib(n-1)     if n>1
-}
 '''
 def fib_recursive(n:int) -> int:
     if n <= 1:
@@ -18,12 +13,12 @@ A dynamic programming approach.
 def fib_dynamic(n:int) -> int:
     if n <= 1:
         return n
-    # populate indices 0 and 1
     F = [0, 1]
-    for i in range(2, n + 1):
+
+    for i in range(2, n+1):
         F.append(F[i-2] + F[i-1])
     return F[n]
 
 if __name__ == '__main__':
-    print(fib_recursive(9))
-    print(fib_dynamic(9))
+    print(fib_recursive(35))
+    print(fib_dynamic(35))
